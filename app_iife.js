17c78ade6f1f31eb1a8de07f5172260af0e12263
Mankur11 <mankur@inbox.ru>
2019-01-07 21:53:40 +0300

    Update app_iife.js

--- app_iife.js
+++ app_iife.js
@@ -1,56 +1,71 @@
 (function () {
     var initialGameState = {
-        diceScores: [0, 0],
+        players: [{
+                name: 'player 1',
+                score: 0
+            },
+            {
+                name: 'player 2',
+                score: 0
+            }
+        ],
+
+        currentDiceScore: 0,
         roundDiceScore: 0,
-        activePlayer: 0,
-        isActiveGame: true,
-        firstPlayerScore: 0,
-        secondPlayerScore: 0,
-        firstPlayerCurrent: 0,
-        secondPlayerCurrent: 0,
-        firstPlayerName: 'Player 1',
-        secondPlayerName: 'Player 2',
+        activePlayerIndex: 0,
+        isActiveGame: true
     };
     var gameState = {};
 
-    function addStartNewGameClickListener() {}
-
-    function startNewGame() {}
-
-    function addRollDiceClickListener() {}
-
-    function rollDice() {}
-
-    function addHoldScoreClickListener() {}
-
-    function holdScore() {}
-
-    function init(config) {
-        document.getElementById('score-0').textContent = config.firstPlayerScore;
-        document.getElementById('score-1').textContent = config.secondPlayerScore;
-        document.getElementById('current-0').textContent = config.firstPlayerCurrent;
-        document.getElementById('current-1').textContent = config.secondPlayerCurrent;
-        document.getElementById('name-0').textContent = config.firstPlayerName;
-        document.getElementById('name-1').textContent = config.secondPlayerName;
-        document.querySelector('.player-0-panel').classList.remove('winner');
-        document.querySelector('.player-1-panel').classList.remove('winner');
-        document.querySelector('.player-0-panel').classList.remove('active');
-        document.querySelector('.player-0-panel').classList.add('active');
-        document.querySelector('.player-1-panel').classList.remove('active');
-
+    function generatePlayers(playerNames) {
+        playerNames.map(function (playerName) {
+            return {
+                name: 'player 1',
+                score: 0
+            };
+        });
     }
+    console.log(generatePlayers(['player 1','player 2']));
+    
 
-    init(initialGameState);
+    // function addStartNewGameClickListener() {}
 
-     addStartNewGameClickListener(); 
+    // function startNewGame() {}
 
-     startNewGame(); 
+    // function addRollDiceClickListener() {}
 
-     addRollDiceClickListener(); 
+    // function rollDice() {}
 
-     rollDice(); 
+    // function addHoldScoreClickListener() {}
 
-     addHoldScoreClickListener(); 
+    // function holdScore() {}
 
-     holdScore(); 
+    // function init(config) {
+    //     document.getElementById('score-0').textContent = config.firstPlayerScore;
+    //     document.getElementById('score-1').textContent = config.secondPlayerScore;
+    //     document.getElementById('current-0').textContent = config.firstPlayerCurrent;
+    //     document.getElementById('current-1').textContent = config.secondPlayerCurrent;
+    //     document.getElementById('name-0').textContent = config.firstPlayerName;
+    //     document.getElementById('name-1').textContent = config.secondPlayerName;
+    //     document.querySelector('.player-0-panel').classList.remove('winner');
+    //     document.querySelector('.player-1-panel').classList.remove('winner');
+    //     document.querySelector('.player-0-panel').classList.remove('active');
+    //     document.querySelector('.player-0-panel').classList.add('active');
+    //     document.querySelector('.player-1-panel').classList.remove('active');
+
+    // }
+
+    // init(initialGameState);
+
+    // addStartNewGameClickListener();
+
+    // startNewGame();
+
+    // addRollDiceClickListener();
+
+    // rollDice();
+
+    // addHoldScoreClickListener();
+
+    // holdScore();
 })();
